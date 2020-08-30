6c29ca1107a24cf810bd5145ace027817b8ea90a
Deiner <deiner.ra10@gmail.com>
2020-08-30 12:26:45 -0800

    Se creo la clase interfaz IPayment

---
+++ src/IPaymentMethod.cpp
@@ -0,0 +1,7 @@
+//
+// Created by deine on 8/30/2020.
+//
+
+#include "IPaymentMethod.h"
+
+IPaymentMethod::~IPaymentMethod(){  }
---
+++ src/IPaymentMethod.h
@@ -0,0 +1,17 @@
+//
+// Created by deine on 8/30/2020.
+//
+
+#ifndef MY_PROJECT_NAME_IPAYMENTMETHOD_H
+#define MY_PROJECT_NAME_IPAYMENTMETHOD_H
+
+#include <string>
+
+class IPaymentMethod {
+    virtual ~IPaymentMethod();
+
+    virtual std::string sendPayment() = 0;
+};
+
+
+#endif //MY_PROJECT_NAME_IPAYMENTMETHOD_H
